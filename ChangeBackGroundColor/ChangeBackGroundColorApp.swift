65e18b7f47c7949e1d9cdb312fa9c773b636d2f6
Grangclark <96944049+Grangclark@users.noreply.github.com>
2025-01-20 23:47:45 +0900

    Initial Commit

---
+++ ChangeBackGroundColor/ChangeBackGroundColorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChangeBackGroundColorApp.swift
+//  ChangeBackGroundColor
+//
+//  Created by 長橋和敏 on 2025/01/20.
+//
+
+import SwiftUI
+
+@main
+struct ChangeBackGroundColorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
